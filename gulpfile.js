c54526a33bd737b5a047f6b8ee1b33703b989ce5
Bujio <javier.vivas@icloud.com>
2024-01-28 15:02:49 +0100

    install gulp and SASS

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+//Vamos a compilar SASS para transformarlo en CSS
+
+const { src, dest, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+function css(cb) {
+  src("./src/scss/**/*.scss") //Identificar el archivo de SASS y su ubicación
+    .pipe(sass()) //Compilarlo con el script del package.json
+    .pipe(dest("build/css")); //Almacenarlo en el disco duro
+
+  cb(); //avisa a gulp de cuando llegamos al final de la tarea
+}
+
+function dev(cb) {
+  watch("./src/scss/**/*.scss", css); // el watch sirve para que esté mirando el archivo css y cada vez que note un cambio, ejecutará la function css
+  cb();
+}
+
+exports.css = css;
+exports.dev = dev;
